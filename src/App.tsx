bc03c51fdac4d787680aa469e3c98c36b3997c22
Seemanta Sarker <91196514+StrangerSeemanta@users.noreply.github.com>
2023-09-04 17:18:43 +0600

    trying

--- src/App.tsx
+++ src/App.tsx
@@ -1,88 +1,33 @@
-import ListGroup from "./components/ListGroup";
-import Alert from "./components/Alert";
-import Button from "./components/Button";
-import Form, { CustomFormData } from "./components/Form";
-import { FormEvent, Fragment, useState } from "react";
-import Game from './tictac'
-import './App.css'
+import React, { Fragment, Component, useLayoutEffect, useRef, useState } from 'react';
+import Navbar from './components/Navbar';
+import Intro from './components/Intro';
+import About from './components/About';
+import Skill from './components/Skill';
+import Projects from './components/Projects';
+import './App.css';
+
 
 function App() {
-  const [visibilityState, setVisibilityState] = useState(false);
-  const [DivisionNameVisible, setDivisionNameVisibl] = useState(false);
-  const [divisionDetails, setDivisionDetails] = useState(<></>)
-  const [formData, setFormData] = useState<CustomFormData>({
-    firstName: " ",
-    lastName: " ",
-    emailId: " ",
-    password: " ",
-    checkMeOut: false
-  })
-  let lists = [
-    "Dhaka",
-    "Khulna",
-    "America",
-    "Sylhet",
-    "Mymensingh",
-    "Rangpur",
-    "Cittagong",
-    "Rajshahi"
 
-  ]
-  let details = [
-    "Dhaka is the capital of Bangladesh",
-    "Khulna is the second modern city in Bangladesh",
-    "Barisal is called second venis town for its riverside views",
-    "Sylhet is a city of Mountain and Naturality",
-    "Mymensingh is newest member of division list for its expansion and modernization",
-    "Rangpur is the place where you can see Kanchan Jangha",
-    "Cittagong is owning the largest sea beach in the world",
-    "Rajshahi the city of mango"
+  const links = ['Home', "About", "Skill", "Projects", "Contact"];
+  const handleNavClick = (item: string, index: number) => {
+    window.location.href = `#${item.toLocaleLowerCase()}`
+  }
+  const logoDetails = {
+    src: '/src/assets/logo.png',
+    altText: "Logo.png"
+  }
 
-  ]
-  const handleSelectedItem = (a: string, i: number) => {
-    let msg = <><h1>{a}</h1><br /><p>{details[i]}</p></>
-    setDivisionDetails(msg)
-    setDivisionNameVisibl(true)
-  }
-  const handleSubmission = (e: FormEvent, data: CustomFormData) => {
-    e.preventDefault();
-    console.log(data)
-    setFormData(data);
-  }
+
   return (
     <Fragment>
-      <section>
-        <div>
-          <ListGroup lists={lists} heading="Divisions" onSelected={handleSelectedItem} />
-          {visibilityState && <Alert withCloseBtn onClose={() => setVisibilityState(false)} children="Hello World" />}
-
-          <Button label="Show Alert" btnType="button" Colors="danger" onClick={() => { setVisibilityState(true) }} />
-
-          {DivisionNameVisible && <Alert withCloseBtn onClose={() => { setDivisionNameVisibl(false); }} children={divisionDetails} />}
-        </div>
-      </section>
-
-      <section>
-        <Form onSubmit={handleSubmission} />
-
-        {formData.firstName.length > 3 && <Alert withCloseBtn={false} onClose={() => { return null; }} children={
-          <>
-            <h1>Hi.. {formData.firstName + " " + formData.lastName}</h1>
-            <br />
-            {formData.emailId.length > 1 ? <p><strong>Your email address: </strong> {formData.emailId}</p> : <i>ENTER YOUR MAIL</i>}
-            <br />
-            <h6>Thanks for submitting your informations</h6>
-          </>}
-        />}
-
-
-      </section>
-      <section>
-        <Game />
-      </section>
-    </Fragment>
-
-  );
+      <Navbar logo={logoDetails} links={links} onSelected={handleNavClick} />
+      <Intro />
+      <About />
+      <Skill />
+      <Projects />
+    </Fragment >
+  )
 }
 
-export default App;
+export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import 'bootstrap/dist/css/bootstrap.css'
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import "aos/dist/aos.css";
+ReactDOM.createRoot(document.getElementById('app') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
